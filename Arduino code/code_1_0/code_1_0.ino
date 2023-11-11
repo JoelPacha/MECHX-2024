7bb2eb8ea25bb5d8c031d4ebb2c403c916d78d67
PachaAfghan <joel.matin2@gmail.com>
2023-11-11 11:01:47 +0100

    sensors code

---
+++ Arduino code/code_1_0/code_1_0.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // ultrasonic sensors output and input 
+  const int trigPin1 = 3;
+  const int echoPin1 = 5; 
+  const int trigPin2 = 6;
+  const int echoPin2 =9; 
+  const int trigPin3 = 10;
+  const int echoPin3 = 11;
+  int irpin =2;
+
+   long sensorTime1;
+   long sensorTime2;
+   long sensorTime3;
+   int distanceLeft;
+   int distanceFront;
+   int distanceRight;
+   int a=0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
